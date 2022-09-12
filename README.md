04d6f7d07b5fca5e206d8daf2c4e20c0515391f8
dev2k <dev2kingdom@gmail.com>
2022-09-12 13:57:15 -0500

    FCMS = CFG - INFO

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # frozen-ac
 80-85 Expansion for WoW 3.3.5a - AerothCore 
+______
+
+
+
+______
+Master Project & Game Server
+
+Website: http://www.dev2kingdom.net/
+Realmlist: logon.dev2kingdom.net
+GitHub : https://github.com/dev2kingdom/frozen-ac
+
+Project Resources & Guides
+
+AzerothCore : https://www.azerothcore.org/
+ModelChanging : https://model-changing.net/
---
+++ _www/games/config.php
@@ -0,0 +1,22 @@
+<?php
+
+$mysql = array(
+
+	"host" => "localhost",
+	"username" => "acore",
+	"password" => "acore",
+	"realmd" => "acore_auth",
+
+);
+
+$site = array(
+
+	"title" => "Dev2Kingdom - Frozen King Registration Page",
+	"meta_description" => "Dev2Kingdom - Frozen King Account Registration",
+	"meta_keywords" => "Dev2Kingdom, Custom Content, WOTLK, Registration",
+	"meta_robots" => "INDEX,NOFOLLOW",
+	"realmlist" => "set realmlist logon.dev2kingdom.net"
+
+);
+
+?>
